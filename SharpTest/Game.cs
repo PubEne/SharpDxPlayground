c24c8661488880473fd73625aab3b748420f2f4c
Piotrek Pawelec <pawelec.piotrek@gmail.com>
2018-12-02 18:59:15 +0100

    "Initial"

---
+++ SharpTest/Game.cs
@@ -0,0 +1,45 @@
+﻿using SharpDX.Windows;
+using System;
+using System.Drawing;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SharpTest
+{
+    public class Game : IDisposable
+    {
+        private RenderForm renderForm;
+
+        private const int Width = 1280;
+        private const int Height = 720;
+
+
+        public Game()
+        {
+            renderForm = new RenderForm("hello");
+            renderForm.ClientSize = new Size(Width, Height);
+            renderForm.AllowUserResizing = false;
+        }
+
+
+        //render game loop
+        public void Run() {
+            RenderLoop.Run(renderForm, RenderCallback);
+        }
+
+        //callled every frame
+        void RenderCallback()
+        {
+
+        }
+
+
+        // dispose unused objects
+        public void Dispose() {
+            renderForm.Dispose();
+        }
+
+    }
+}
